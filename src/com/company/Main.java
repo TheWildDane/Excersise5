5681e6b0c61fa3e2c96b4a88221c590e0a87fa8e
Steffen Rasmussen <42993915+TheWildDane@users.noreply.github.com>
2018-09-20 12:37:23 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+//calculate float and output answer
+        System.out.print("Answer for (7.5*6.5-4.5*3)/(47.5-5.5) is\n");
+        System.out.println((7.5*6.5-4.5*3)/(47.5-5.5));
+    }
+}
